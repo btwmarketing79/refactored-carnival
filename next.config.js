cbb9f808deab3f790ebcb3e03a158e53ab344d03
btwmarketing79 <148632717+btwmarketing79@users.noreply.github.com>
2024-08-27 11:27:45 +0200

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    output: 'export',
+    basePath: '/refactored-carnival',
+    assetPrefix: '/refactored-carnival/'
+  };
+  
+  export default nextConfig;
